3fd8740ce7740ee3409d5948a19dd4cd477cc9af
dimazzers <dimasik1801@gmail.com>
2017-05-22 19:22:54 +0500

    p

--- code.js
+++ code.js
@@ -11,5 +11,18 @@ var time = "двух цифр, например 09:00, 21-30.Напишите р
 var zd_3 = time.match(/\d{2}[:-]\d{2}/g);
 console.log("3. " + zd_3);
 
-var dosk;
-var zd_3 = dosk.match(/[a-h][1-8]/gi);
+window.onload = function () {
+	var table = document.createElement('table');
+
+	for(x=0; x<8; x++) {
+		var tr = document.createElement('tr');
+		for(y=0; y<8; y++){
+			var td = document.createElement('td');
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+	document.body.appendChild(table);
+	console.log("4. ")
+}
+
